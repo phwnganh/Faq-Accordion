f5e86286c2e005c7686a5e3ed72e1617487f98c6
phwnganh <phuonganh25102003@gmail.com>
2026-02-28 18:03:05 +0700

    fix: accessibility of faq content 2

--- src/components/FAQContentItem.jsx
+++ src/components/FAQContentItem.jsx
@@ -6,13 +6,13 @@ const FaqContentItem = ({item, isOpenAnswer, onOpenAnswer}) => {
         <section className={"flex flex-col gap-6"}>
             <hr className={"border-t border-t-purple-100"}/>
             <button type={"button"} aria-expanded="false" aria-controls={`answer-${item.id}`} id={`question-${item.id}`} className={"flex items-center justify-between"}>
-                <h2 onClick={onOpenAnswer} className={`text-preset-2 font-preset-2 leading-preset-2 hover:text-violet-600 cursor-pointer`}>{item.question}</h2>
+                <h2 onClick={onOpenAnswer} className={`text-preset-2 font-preset-2 leading-preset-2 hover:text-violet-600 focus:text-violet-600 cursor-pointer`}>{item.question}</h2>
                 <div className={"flex justify-center items-center shrink-0"}>
                     <img src={isOpenAnswer ? MinusIcon : PlusIcon} alt={'icon'}/>
                 </div>
             </button>
             {isOpenAnswer && (
-                <p aria-expanded aria-labelledby={`question-${item.id}`} role={"region"} className={"text-preset-3 leading-preset-3 text-purple-600"}>{item.answer}</p>
+                <p id={`answer-${item.id}`} aria-expanded aria-labelledby={`question-${item.id}`} role={"region"} className={"text-preset-3 leading-preset-3 text-purple-600"}>{item.answer}</p>
             )}
         </section>
     );
