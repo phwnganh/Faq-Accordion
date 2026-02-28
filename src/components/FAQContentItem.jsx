937fbf3684c82162adcdabaf9d5ec686187dc85d
phwnganh <phuonganh25102003@gmail.com>
2026-02-28 17:41:25 +0700

    fix: accessibility of the faq content

--- src/components/FAQContentItem.jsx
+++ src/components/FAQContentItem.jsx
@@ -6,13 +6,13 @@ const FaqContentItem = ({item, isOpenAnswer, onOpenAnswer}) => {
         <section className={"flex flex-col gap-6"}>
             <hr className={"border-t border-t-purple-100"}/>
             <div className={"flex items-center justify-between"}>
-                <h3 onClick={onOpenAnswer} className={`text-preset-2 font-preset-2 leading-preset-2 hover:text-violet-600 cursor-pointer`}>{item.question}</h3>
+                <h3 aria-controls aria-labelledby={"question"} tabIndex={0} role={"button"} onClick={onOpenAnswer} className={`text-preset-2 font-preset-2 leading-preset-2 hover:text-violet-600 cursor-pointer`}>{item.question}</h3>
                 <div className={"flex justify-center items-center shrink-0"}>
                     <img src={isOpenAnswer ? MinusIcon : PlusIcon} alt={'icon'}/>
                 </div>
             </div>
             {isOpenAnswer && (
-                <p className={"text-preset-3 leading-preset-3 text-purple-600"}>{item.answer}</p>
+                <p aria-expanded aria-labelledby={"answer"} className={"text-preset-3 leading-preset-3 text-purple-600"}>{item.answer}</p>
             )}
             <div></div>
         </section>
